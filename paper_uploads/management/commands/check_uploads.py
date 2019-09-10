dca8ac0f523b3288bc7ca1f5383adac9e4e87adf
pix666 <6928240+pix666@users.noreply.github.com>
2019-09-10 11:02:35 +0400

    refactor `check_variations` command

--- paper_uploads/management/commands/check_uploads.py
+++ paper_uploads/management/commands/check_uploads.py
@@ -5,9 +5,9 @@ from ...models import UploadedImageBase
 
 
 class Command(BaseCommand):
+    options = None
     verbosity = None
     database = DEFAULT_DB_ALIAS
-    help = 'Check if all variation files exists'
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -16,41 +16,51 @@ class Command(BaseCommand):
         )
         parser.add_argument(
             '--fix-missing', action='store_true', default=False,
-            help='Create all missing variation files from source.',
+            help='Recreate all missing variation files from source.',
         )
 
-    def handle(self, *args, **options):
-        self.verbosity = options['verbosity']
-        self.database = options['database']
-
+    def check_variations(self):
         for model in apps.get_models():
             if not issubclass(model, UploadedImageBase):
                 continue
 
-            for instance in model._meta.base_manager.all():
+            for instance in model._meta.base_manager.using(self.database).all():
                 missed_original = not instance.file.storage.exists(instance.file.name)
                 missed_variations = []
                 for vname, vfile in instance.get_variation_files():
                     if not vfile.exists():
                         missed_variations.append(vname)
 
-                message = "Error on '{}.{}' (#{})".format(
+                invalid = False
+                recreatable = self.options['fix_missing'] and not missed_original
+                message = "Errors were found in '{}.{}' #{}:".format(
                     model._meta.app_label,
                     model._meta.model_name,
                     instance.pk
                 )
+
                 if missed_original:
-                    self.stderr.write(
-                        self.style.ERROR(
-                            "{}:  Not found source file".format(message)
-                        )
-                    )
+                    invalid = True
+                    message += "\n  Not found source file"
+
                 if missed_variations:
+                    invalid = True
                     for vname in missed_variations:
-                        self.stderr.write(
-                            self.style.WARNING(
-                                "{}:  Not found variation: {}".format(message, vname)
-                            )
-                        )
-                    if options['fix_missing'] and not missed_original:
+                        message += "\n  Not found variation '{}'".format(vname)
+                        if recreatable:
+                            message += " (recreated)"
+
+                    if recreatable:
                         instance.recut(names=missed_variations)
+
+                if invalid:
+                    self.stdout.write(
+                        self.style.ERROR(message)
+                    )
+
+    def handle(self, *args, **options):
+        self.options = options
+        self.verbosity = options['verbosity']
+        self.database = options['database']
+
+        self.check_variations()
