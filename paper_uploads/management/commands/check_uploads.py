61dcc4e1fc1d141ba3f6a016859f05dc1ff388f5
pix666 <6928240+pix666@users.noreply.github.com>
2019-09-12 16:39:19 +0400

    add owner checking

--- paper_uploads/management/commands/check_uploads.py
+++ paper_uploads/management/commands/check_uploads.py
@@ -1,7 +1,8 @@
 from django.apps import apps
-from django.core.management import BaseCommand
 from django.db import DEFAULT_DB_ALIAS
-from ...models import UploadedImageBase
+from django.core.management import BaseCommand
+from ...models.base import SlaveModelMixin
+from ...models import UploadedImageBase, Gallery
 
 
 class Command(BaseCommand):
@@ -54,13 +55,80 @@ class Command(BaseCommand):
                         instance.recut(names=missed_variations)
 
                 if invalid:
-                    self.stdout.write(
-                        self.style.ERROR(message)
+                    self.stdout.write(self.style.ERROR(message))
+
+    def check_owner(self):
+        for model in apps.get_models():
+            if not issubclass(model, SlaveModelMixin):
+                continue
+
+            if model._meta.proxy:
+                continue
+
+            total = model._meta.base_manager.count()
+            if not total:
+                continue
+
+            for index, instance in enumerate(model._meta.base_manager.iterator(), start=1):
+                self.stdout.write('\r' + (' ' * 80), ending='\r')
+
+                real_model = model
+                if isinstance(instance, Gallery):
+                    real_model = instance.gallery_content_type.model_class()
+
+                invalid = False
+                message = "Errors were found in '{}.{}' #{}:".format(
+                    real_model._meta.app_label,
+                    real_model._meta.model_name,
+                    instance.pk
+                )
+
+                owner_model = instance.get_owner_model()
+                if owner_model is None:
+                    invalid = True
+                    message += "\n  Owner model '{}.{}' doesn't exists".format(
+                        instance.owner_app_label,
+                        instance.owner_model_name,
                     )
+                else:
+                    owner_field = instance.get_owner_field()
+                    if owner_field is None:
+                        invalid = True
+                        message += "\n  Owner model '{}.{}' has no field named '{}'".format(
+                            instance.owner_app_label,
+                            instance.owner_model_name,
+                            instance.owner_fieldname,
+                        )
+                    else:
+                        try:
+                            owner_model._meta.base_manager.get(
+                                (instance.owner_fieldname, instance.pk)
+                            )
+                        except owner_model.DoesNotExist:
+                            invalid = True
+                            message += "\n  Owner instance '{}.{}' not found".format(
+                                instance.owner_app_label,
+                                instance.owner_model_name,
+                            )
+
+                if invalid:
+                    self.stdout.write(self.style.ERROR(message))
+
+                self.stdout.write(self.style.SUCCESS(
+                    "Checked owner of '{}.{}' ({}/{})\r".format(
+                        model._meta.app_label,
+                        model._meta.model_name,
+                        index,
+                        total
+                    )
+                ), ending='')
+
+            self.stdout.write('')
 
     def handle(self, *args, **options):
         self.options = options
         self.verbosity = options['verbosity']
         self.database = options['database']
 
+        self.check_owner()
         self.check_variations()
