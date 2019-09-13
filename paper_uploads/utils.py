483e1894757180063754cef74f1b405fc5223daa
pix666 <6928240+pix666@users.noreply.github.com>
2019-09-13 10:34:10 +0400

    remove stale function

--- paper_uploads/utils.py
+++ paper_uploads/utils.py
@@ -1,5 +1,3 @@
-from django.apps import apps
-from django.core.exceptions import FieldDoesNotExist
 from variations.variation import Variation
 from .conf import settings
 
@@ -11,19 +9,3 @@ def build_variations(options):
             config.setdefault('face_detection', True)
         variations[key] = Variation(**config)
     return variations
-
-
-def get_field(app_label, model_name, fieldname):
-    """
-    Получение поля модели.
-    """
-    try:
-        model_class = apps.get_model(app_label, model_name)
-    except LookupError:
-        logger.exception("Not found model: %s.%s" % (app_label, model_name))
-        return
-
-    try:
-        return model_class._meta.get_field(fieldname)
-    except FieldDoesNotExist:
-        logger.exception("Not found field '%s' in model %s.%s" % (app_label, model_name, fieldname))
