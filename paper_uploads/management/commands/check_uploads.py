6fffdf4a21afe3d033c57a4e7a9320708928bb56
pix666 <6928240+pix666@users.noreply.github.com>
2019-09-17 10:00:45 +0400

    check item_type values in check_uploads command

--- paper_uploads/management/commands/check_uploads.py
+++ paper_uploads/management/commands/check_uploads.py
@@ -2,7 +2,7 @@ from django.apps import apps
 from django.db import DEFAULT_DB_ALIAS
 from django.core.management import BaseCommand
 from ...models.base import SlaveModelMixin
-from ...models import UploadedImageBase, Gallery
+from ...models import UploadedImageBase, Gallery, GalleryItemBase
 
 
 class Command(BaseCommand):
@@ -25,25 +25,31 @@ class Command(BaseCommand):
             if not issubclass(model, UploadedImageBase):
                 continue
 
-            for instance in model._meta.base_manager.using(self.database).all():
-                missed_original = not instance.file.storage.exists(instance.file.name)
-                missed_variations = []
-                for vname, vfile in instance.get_variation_files():
-                    if not vfile.exists():
-                        missed_variations.append(vname)
+            total = model._meta.base_manager.using(self.database).count()
+            if not total:
+                continue
+
+            for index, instance in enumerate(model._meta.base_manager.using(self.database).iterator(), start=1):
+                self.stdout.write('\r' + (' ' * 80), ending='\r')
 
                 invalid = False
-                recreatable = self.options['fix_missing'] and not missed_original
                 message = "Errors were found in '{}.{}' #{}:".format(
                     model._meta.app_label,
                     model._meta.model_name,
                     instance.pk
                 )
 
+                missed_original = not instance.file.storage.exists(instance.file.name)
+                missed_variations = []
+                for vname, vfile in instance.get_variation_files():
+                    if not vfile.exists():
+                        missed_variations.append(vname)
+
                 if missed_original:
                     invalid = True
                     message += "\n  Not found source file"
 
+                recreatable = self.options['fix_missing'] and not missed_original
                 if missed_variations:
                     invalid = True
                     for vname in missed_variations:
@@ -57,7 +63,18 @@ class Command(BaseCommand):
                 if invalid:
                     self.stdout.write(self.style.ERROR(message))
 
-    def check_owner(self):
+                self.stdout.write(self.style.SUCCESS(
+                    "Check files of '{}.{}' ({}/{}) ...\r".format(
+                        model._meta.app_label,
+                        model._meta.model_name,
+                        index,
+                        total
+                    )
+                ), ending='')
+
+            self.stdout.write('')
+
+    def check_owners(self):
         for model in apps.get_models():
             if not issubclass(model, SlaveModelMixin):
                 continue
@@ -65,11 +82,11 @@ class Command(BaseCommand):
             if model._meta.proxy:
                 continue
 
-            total = model._meta.base_manager.count()
+            total = model._meta.base_manager.using(self.database).count()
             if not total:
                 continue
 
-            for index, instance in enumerate(model._meta.base_manager.iterator(), start=1):
+            for index, instance in enumerate(model._meta.base_manager.using(self.database).iterator(), start=1):
                 self.stdout.write('\r' + (' ' * 80), ending='\r')
 
                 real_model = model
@@ -115,7 +132,7 @@ class Command(BaseCommand):
                     self.stdout.write(self.style.ERROR(message))
 
                 self.stdout.write(self.style.SUCCESS(
-                    "Checked owner of '{}.{}' ({}/{})\r".format(
+                    "Check owner of '{}.{}' ({}/{}) ...\r".format(
                         model._meta.app_label,
                         model._meta.model_name,
                         index,
@@ -125,10 +142,55 @@ class Command(BaseCommand):
 
             self.stdout.write('')
 
+    def check_item_types(self):
+        total = GalleryItemBase.objects.using(self.database).count()
+        for index, item in enumerate(GalleryItemBase.objects.using(self.database).iterator(), start=1):
+            self.stdout.write('\r' + (' ' * 80), ending='\r')
+
+            invalid = False
+            message = "Errors were found in '{}.{}' #{}:".format(
+                item._meta.app_label,
+                item._meta.model_name,
+                item.pk
+            )
+
+            gallery_cls = item.get_gallery_class()
+            if item.item_type not in gallery_cls.item_types:
+                invalid = True
+                message += "\n  Item type '{}' is not defined in gallery '{}.{}' #{}".format(
+                    item.item_type,
+                    gallery_cls._meta.app_label,
+                    gallery_cls._meta.model_name,
+                    item.object_id,
+                )
+            else:
+                item_model = gallery_cls.item_types[item.item_type].model
+                if item_model is not type(item):
+                    invalid = True
+                    message += "\n  Item class '{}.{}' is different from the '{}.{}'".format(
+                        item._meta.app_label,
+                        item._meta.model_name,
+                        gallery_cls._meta.app_label,
+                        gallery_cls._meta.model_name,
+                    )
+
+            if invalid:
+                self.stdout.write(self.style.ERROR(message))
+
+            self.stdout.write(self.style.SUCCESS(
+                "Check item_type of gallery items ({}/{}) ...\r".format(
+                    index,
+                    total
+                )
+            ), ending='')
+
+        self.stdout.write('')
+
     def handle(self, *args, **options):
         self.options = options
         self.verbosity = options['verbosity']
         self.database = options['database']
 
-        self.check_owner()
+        self.check_item_types()
+        self.check_owners()
         self.check_variations()
