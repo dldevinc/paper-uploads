b2438f8965ef184e474a59752a0c926f47da9edd
pix666 <6928240+pix666@users.noreply.github.com>
2019-10-17 13:55:11 +0400

    add `postprocess` to FileField and CollectionItemTypeField

--- paper_uploads/models/fields/file.py
+++ paper_uploads/models/fields/file.py
@@ -4,9 +4,16 @@ from ... import forms
 
 
 class FileField(FileFieldBase):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, postprocess=None, **kwargs):
         kwargs.setdefault('to', UploadedFile)
         super().__init__(*args, **kwargs)
+        self.postprocess = postprocess
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        if 'postprocess' in kwargs:
+            del kwargs['postprocess']
+        return name, path, args, kwargs
 
     def formfield(self, **kwargs):
         return super().formfield(**{
