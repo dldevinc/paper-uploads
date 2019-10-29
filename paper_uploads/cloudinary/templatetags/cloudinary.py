a30b4714fc305393c104d3ba77d6d3f157c40188
pix666 <6928240+pix666@users.noreply.github.com>
2019-10-29 17:17:31 +0400

    add cloudinary templatetag

---
+++ paper_uploads/cloudinary/templatetags/cloudinary.py
@@ -0,0 +1,59 @@
+from functools import partial
+from cloudinary.templatetags.cloudinary import cloudinary_url
+from ..container import CloudinaryContainerMixin
+
+
+try:
+    import jinja2
+except ImportError:
+    jinja2 = None
+
+
+if jinja2 is not None:
+    from jinja2.ext import Extension, nodes
+
+    def paper_cloudinary(func):
+        """
+        Декоратор для функций модуля cloudinary, позволяющий передавать в качестве
+        source экземпляры CloudinaryContainerMixin.
+        """
+        def inner(ctx, source, *args, **kwargs):
+            if isinstance(source, CloudinaryContainerMixin):
+                source = source.file
+            return func(ctx, source, *args, **kwargs)
+        return inner
+
+
+    class CloudinaryExtension(Extension):
+        tags = {'cloudinary_url'}
+
+        def parse(self, parser):
+            lineno = next(parser.stream).lineno
+            args = [nodes.ContextReference()]
+            kwargs = []
+            while parser.stream.current.type != 'block_end':
+                if parser.stream.current.type == 'name' and parser.stream.look().type == 'assign':
+                    name = parser.stream.expect('name')
+                    parser.stream.expect('assign')
+                    value = parser.parse_expression()
+                    kwargs.append(nodes.Keyword(name.value, value, lineno=value.lineno))
+                else:
+                    args.append(parser.parse_expression())
+            return nodes.CallBlock(self.call_method('_cloudinary_url', args, kwargs), [], [], []).set_lineno(lineno)
+
+        @staticmethod
+        def _cloudinary_url(ctx, *args, caller=None, **kwargs):
+            return paper_cloudinary(cloudinary_url)(ctx, *args, **kwargs)
+
+
+    # django-jinja support
+    try:
+        from django_jinja import library
+    except ImportError:
+        pass
+    else:
+        dummy_ctx = {}
+        cloudinary_url = partial(paper_cloudinary(cloudinary_url), dummy_ctx)
+        library.global_function(name='cloudinary_url')(cloudinary_url)
+
+        library.extension(CloudinaryExtension)
