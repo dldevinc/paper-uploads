656ccb13425802a6451985dee999f4559942c9ea
pix666 <6928240+pix666@users.noreply.github.com>
2020-07-31 18:08:23 +0400

    add storage fixture

--- tests/tests/conftest.py
+++ tests/tests/conftest.py
@@ -1,9 +1,14 @@
+from threading import local
+
 import pytest
 import pytest_django.fixtures
 
 
 @pytest.fixture(scope="class")
 def class_scoped_db(request, django_db_setup, django_db_blocker):
+    """
+    Like `db` fixture, but class-scoped.
+    """
     if "django_db_reset_sequences" in request.fixturenames:
         request.getfixturevalue("django_db_reset_sequences")
     if (
@@ -13,3 +18,41 @@ def class_scoped_db(request, django_db_setup, django_db_blocker):
         request.getfixturevalue("transactional_db")
     else:
         pytest_django.fixtures._django_db_fixture_helper(request, django_db_blocker, transactional=False)
+
+
+@pytest.fixture(scope='class')
+def storage(request, class_scoped_db):
+    """
+    Инициализация тестируемых объектов для класса.
+    В отличие от fixture, позволяет использовать
+    наследование классов, наследуя тем самым методы,
+    и, в то же время, переопределять тестируемый
+    объект.
+
+        class TestShape:
+            @staticmethod
+            def init(storage):
+                storage.object = Shape(...)
+                yield
+                storage.object.delete()
+
+            def test_smthg(self, storage):
+                assert storage.object.pk == 1
+
+        class TestSquare(TestShape):
+            @staticmethod
+            def init(storage):
+                storage.object = Square(...)
+                yield
+                storage.object.delete()
+    """
+    storage = local()
+    gen = request.cls.init(storage)
+
+    next(gen)
+    yield storage
+
+    try:
+        next(gen)
+    except StopIteration:
+        pass
