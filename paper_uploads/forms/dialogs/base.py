92b0a01afce1c500957d55580f75adb2dee92b9e
pix666 <6928240+pix666@users.noreply.github.com>
2020-08-04 12:41:59 +0400

    Revert "remove form renderer"
    This reverts commit 2a12bd82869447f567ced773a8687b23676af50f.

--- paper_uploads/forms/dialogs/base.py
+++ paper_uploads/forms/dialogs/base.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from paper_admin.renderer import PaperFormRenderer
 
 
 class UploadedFileBaseForm(forms.ModelForm):
-    # TODO: form renderer
+    default_renderer = PaperFormRenderer
     new_name = forms.CharField(required=True, label=_('File name'), max_length=255,)
 
     class Meta:
