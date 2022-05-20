0738e865f909dc1dfde3067827f9d1e63209c237
pix666 <6928240+pix666@users.noreply.github.com>
2022-05-20 10:51:48 +0400

    run prettier

--- paper_uploads/static/paper_uploads/src/js/widget.js
+++ paper_uploads/static/paper_uploads/src/js/widget.js
@@ -8,24 +8,20 @@ function importAll(r) {
 }
 importAll(require.context("../img/files/", false, /\.svg$/));
 
-
 const fileWidget = new file.FileUploaderWidget();
 fileWidget.observe(".file-uploader");
 fileWidget.initAll(".file-uploader");
-
 
 const imageWidget = new image.ImageUploaderWidget();
 imageWidget.observe(".image-uploader");
 imageWidget.initAll(".image-uploader");
 
-
 const collectionWidget = new collection.CollectionWidget();
 collectionWidget.observe(".collection");
 collectionWidget.initAll(".collection");
-
 
 export const paperUploads = {
     file,
     image,
     collection
-}
+};
