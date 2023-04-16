85a8be411dd20458823845ecb0cda0c516f6a7c7
pix666 <x896321475@gmail.com>
2023-04-16 20:22:39 +0400

    update environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,140 +1,156 @@
-const path = require("path");
-const webpack = require("webpack");
-const pixrem = require("pixrem");
-const autoprefixer = require("autoprefixer");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+import path from "path";
+import webpack from "webpack";
+import { fileURLToPath } from "url";
+import { merge } from "webpack-merge";
+import TerserPlugin from "terser-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
-const SOURCE_DIR = "paper_uploads/static/paper_uploads/src";
-const DIST_DIR = "paper_uploads/static/paper_uploads/dist";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-let config = {
-    entry: {
-        widget: path.resolve(SOURCE_DIR, "js/widget.js")
-    },
-    output: {
-        clean: true,
-        path: path.resolve(DIST_DIR),
-        publicPath: "/static/paper_uploads/dist/",
-        filename: "[name].js",
-        assetModuleFilename: "assets/[name][ext][query]",
-        library: {
-            type: "window"
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /[\\/]node_modules[\\/]/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
+const SOURCE_DIR = path.resolve(__dirname, "paper_uploads/static/paper_uploads/src");
+const DIST_DIR = path.resolve(__dirname, "paper_uploads/static/paper_uploads/dist");
+
+// Базовый объект, чьи свойства наследуют все конфигурации.
+function getCommonConfig(devMode) {
+    return {
+        mode: devMode ? "development" : "production",
+        devtool: devMode ? "eval" : "source-map",
+        cache: devMode
+            ? {
+                type: "filesystem",
+                cacheDirectory: path.resolve(__dirname, "cache"),
+                buildDependencies: {
+                    config: [__filename]
+                }
+            }
+            : false,
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /[\\/]node_modules[\\/]/,
+                    loader: "babel-loader",
+                    options: devMode
+                        ? {
                             cacheDirectory: path.resolve(__dirname, "cache")
                         }
-                    }
-                ]
-            },
+                        : {}
+                },
 
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "fast-css-loader"
-                    }
-                ]
-            },
-
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "fast-css-loader",
-                        options: {
-                            importLoaders: 2
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: "fast-css-loader",
+                            options: {
+                                importLoaders: 1
+                            }
+                        },
+                        {
+                            loader: "postcss-loader"
                         }
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [pixrem(), autoprefixer()]
+                    ]
+                },
+
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: "fast-css-loader",
+                            options: {
+                                importLoaders: 2
+                            }
+                        },
+                        {
+                            loader: "postcss-loader"
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sassOptions: {
+                                    includePaths: [
+                                        path.resolve(SOURCE_DIR, "css"),
+                                        path.resolve(__dirname, "node_modules")
+                                    ]
+                                }
                             }
                         }
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sassOptions: {
-                                includePaths: [path.resolve(SOURCE_DIR, "css"), path.resolve(__dirname, "node_modules")]
-                            }
-                        }
-                    }
-                ]
-            },
+                    ]
+                },
 
-            {
-                test: /\.(jpe?g|png|gif|woff2?|ttf|eot|svg)$/i,
-                type: "asset/resource"
+                {
+                    test: /\.(jpe?g|png|gif|woff2?|ttf|eot|svg)$/i,
+                    type: "asset/resource"
+                },
+
+                // https://webpack.js.org/guides/asset-modules/#replacing-inline-loader-syntax
+                {
+                    resourceQuery: /raw/,
+                    type: "asset/source"
+                }
+            ]
+        },
+        resolve: {
+            modules: [SOURCE_DIR, "node_modules"]
+        },
+        plugins: [
+            new webpack.ProgressPlugin(),
+            new MiniCssExtractPlugin({
+                filename: "[name].css"
+            })
+        ],
+        optimization: {
+            moduleIds: "deterministic",
+            minimizer: [].concat(
+                devMode
+                    ? []
+                    : [
+                        new TerserPlugin({
+                            parallel: true
+                        }),
+                        new CssMinimizerPlugin({})
+                    ]
+            )
+        },
+        watchOptions: {
+            aggregateTimeout: 2000,
+            ignored: ["**/node_modules"]
+        },
+        stats: {
+            assets: false,
+            chunks: true
+        }
+    };
+}
+
+// Конфигурация общих ресурсов, которые будут загружены в defer-режиме.
+function getAppConfig(devMode) {
+    return merge(getCommonConfig(devMode), {
+        entry: {
+            widget: path.resolve(SOURCE_DIR, "js/widget.js")
+        },
+        output: {
+            clean: true,
+            path: path.resolve(DIST_DIR),
+            publicPath: "/static/paper_uploads/dist/",
+            filename: "[name].js",
+            assetModuleFilename: "assets/[name][ext][query]",
+            library: {
+                type: "window"
             }
-        ]
-    },
-    resolve: {
-        modules: [SOURCE_DIR, "node_modules"]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].css"
-        })
-    ],
-    optimization: {
-        moduleIds: "deterministic"
-    },
-    watchOptions: {
-        aggregateTimeout: 2000,
-        ignored: ["**/node_modules"]
-    },
-    stats: {
-        assets: false,
-        chunks: true
-    }
-};
+        }
+    });
+}
 
-module.exports = (env, argv) => {
-    config.mode = argv.mode === "production" ? "production" : "development";
-
-    if (config.mode === "production") {
-        config.devtool = "source-map";
-    } else {
-        config.devtool = "eval";
-    }
-
-    if (config.mode === "development") {
-        config.cache = {
-            type: "filesystem",
-            cacheDirectory: path.resolve(__dirname, "cache"),
-            buildDependencies: {
-                config: [__filename]
-            }
-        };
-    }
-
-    if (config.mode === "production") {
-        config.optimization.minimizer = [
-            new TerserPlugin({
-                parallel: true
-            }),
-            new CssMinimizerPlugin({})
-        ];
-    }
-
-    return config;
+export default (env, argv) => {
+    const devMode = argv.mode !== "production";
+    return [getAppConfig(devMode)];
 };
